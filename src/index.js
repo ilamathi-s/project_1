c294f8d743d5a8aa9ea28b44efdffa2323b65fc1
ilamathi-s <ilamathi0005@gmail.com>
2024-08-04 17:21:02 +0530

    Updated

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-// import ReactDOM from "react-dom";
+import ReactDOM from "react-dom";
 import React from "react";
 import App from "./App";
-React.render(<App/>,document.getElementById("App"));
+import { BrowserRouter } from "react-router-dom";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
