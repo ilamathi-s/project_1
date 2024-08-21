2ccbeefbff1ffec0f94d17103c4a52e1b3cbf8a6
ilamathi-s <ilamathi0005@gmail.com>
2024-08-21 19:58:28 +0530

    Updated css file

--- src/Auth/Signin/Signin.js
+++ src/Auth/Signin/Signin.js
@@ -1,19 +1,29 @@
-import React from "react";
+import React,{useState} from "react";
+import Axios from 'axios' 
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import {Link} from "react-router-dom";
 import '../Signin/Signin.css';
 function Signin() {
+  const [mail,setMail]=useState("");
+  const [password,setPassword]=useState("");
+  const Signin = () =>{
+    Axios.post('http://localhost:3000/Signin', {
+      email: mail,
+      password: password }).then((res) => {console.log(res);});
+
+  }
   return (
     <div className="mb-3"> 
-      <h2>Sign In</h2>
-        <Form className="form" >
-          <input htmlFor="mail" placeholder="Enter valid mail id"   required /><br/><br/>
-          <input type="password" htmlFor="password" placeholder="Enter valid Password"  required/><br/><br/>
-          <Button type="submit" variant="outline-success">Sign In</Button><br/>
+        <Form className="form">
+        <h2 >Sign In</h2>
+          <input type="text" onChange={(m) => {setMail(m.target.value)}} htmlFor="mail" placeholder="Mail id"   required /><br/><br/>
+          <input type="password" onChange={(p) => {setPassword(p.target.value)}} htmlFor="password" placeholder="Password"  required/><br/><br/>
+          <Button type="submit" onClick={Signin} variant="outline-success">Sign In</Button><br/>
           <Link to="/Signup" variant="link"  >New User</Link> 
           <Link to="/Forgotpassword" variant="link" className="spacing">Forget Password</Link>
         </Form>
+
     </div>
   );
 }
